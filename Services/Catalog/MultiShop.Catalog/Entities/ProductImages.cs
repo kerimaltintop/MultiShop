ebcdaadfd001400cce3809b61065e9b8279bd295
Kerim ALTINTOP <kerimaltintop@gmail.com>
2025-06-04 14:18:53 +0300

    first commit

---
+++ Services/Catalog/MultiShop.Catalog/Entities/ProductImages.cs
@@ -0,0 +1,14 @@
+﻿namespace MultiShop.Catalog.Entities
+{
+    public class ProductImages
+    {
+        public string ProductImagesId { get; set; }
+
+        public string Image1 { get; set; }
+        public string Image2 { get; set; }
+        public string Image3 { get; set; }
+
+        public string ProductId { get; set; }
+        public Product Product { get; set; }
+    }
+}
